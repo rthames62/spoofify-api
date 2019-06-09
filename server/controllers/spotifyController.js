c5a0baa10529bb6670ece959b50b482ec826be8b
Ryan Thames <ryan@Ryans-MacBook-Pro.local>
2019-06-08 21:49:03 -0400

    first commit

---
+++ server/controllers/spotifyController.js
@@ -0,0 +1,175 @@
+// const keys = require('../keys');
+const request = require('request');
+const baseUri = 'https://api.spotify.com/v1';
+
+const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+        'Authorization': 'Basic ' + (new Buffer(process.env.clientid + ':' + process.env.clientkey).toString('base64'))
+    },
+    form: {
+        grant_type: 'client_credentials'
+    },
+    json: true
+}
+const authHeaders = {
+    headers: {
+        'Authorization' : ''
+    },
+    json: true
+};
+
+const authorizeSpotify = () => {
+    request.post(authOptions, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            authHeaders.headers.Authorization = `Bearer ${body.access_token}`;
+        }
+    });
+}
+
+const getCategories = (req, res) => {
+    request.get(`${baseUri}/browse/categories`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(err);
+        }
+    })
+}
+
+const getFeaturedPlaylists = (req, res) => {
+    request.get(`${baseUri}/browse/featured-playlists`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json('Error');
+        }
+    })
+}
+
+const getRecommendations = (req, res) => {
+    request.get(`${baseUri}/recommendations`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(err);
+        }
+    })
+}
+
+const getPlaylistById = (req, res) => {
+    request.get(`${baseUri}/playlists/${req.params.id}`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(err);
+        }
+    })
+}
+
+const getArtistById = (req, res) => {
+    request.get(`${baseUri}/artists/${req.params.id}`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(err);
+        }
+    })
+}
+
+const getArtistAlbums = (req, res) => {
+    request.get(`${baseUri}/artists/${req.params.id}/albums?market=US&include_groups=album`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(err);
+        }
+    })
+}
+
+const getArtistSingles = (req, res) => {
+    request.get(`${baseUri}/artists/${req.params.id}/albums?market=US&include_groups=single`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(err);
+        }
+    })
+}
+
+const getArtistCompilations = (req, res) => {
+    request.get(`${baseUri}/artists/${req.params.id}/albums?market=US&include_groups=compilation`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(err);
+        }
+    })
+}
+
+const getArtistAppearsOn = (req, res) => {
+    request.get(`${baseUri}/artists/${req.params.id}/albums?market=US&include_groups=appears_on`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(err);
+        }
+    })
+}
+
+const getArtistTopTracks = (req, res) => {
+    request.get(`${baseUri}/artists/${req.params.id}/top-tracks?country=us`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(response);
+        }
+    })
+}
+
+const getAlbumById = (req, res) => {
+    request.get(`${baseUri}/albums/${req.params.id}?market=us`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(response);
+        }
+    })
+}
+
+const getArtistRelatedArtists = (req, res) => {
+    request.get(`${baseUri}/artists/${req.params.id}/related-artists`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(response);
+        }
+    })
+}
+
+const searchSpotify = (req, res) => {
+    request.get(`${baseUri}/search?q=${req.params.q}*&type=album,artist,playlist,track&market=us&limit=20&best_match=true`, authHeaders, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+            res.json(body);
+        } else {
+            res.json(response);
+        }
+    })
+}
+
+module.exports = {
+    authorizeSpotify: authorizeSpotify,
+    getCategories: getCategories,
+    getFeaturedPlaylists: getFeaturedPlaylists,
+    getRecommendations: getRecommendations,
+    getPlaylistById: getPlaylistById,
+    getArtistById: getArtistById,
+    getArtistTopTracks: getArtistTopTracks,
+    getArtistAlbums: getArtistAlbums,
+    getArtistSingles: getArtistSingles,
+    getArtistCompilations: getArtistCompilations,
+    getArtistAppearsOn: getArtistAppearsOn,
+    getAlbumById: getAlbumById,
+    getArtistRelatedArtists: getArtistRelatedArtists,
+    searchSpotify: searchSpotify
+}
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+
+const spotifyController = require('./controllers/spotifyController');
+
+app.use(cors());
+spotifyController.authorizeSpotify();
+
+console.log('API 11111111111111');
+
+app.get('/api/spotify/categories', spotifyController.getCategories);
+app.get('/api/spotify/featured-playlists', spotifyController.getFeaturedPlaylists);
+app.get('/api/spotify/recommendations', spotifyController.getRecommendations);
+app.get('/api/spotify/playlist/:id', spotifyController.getPlaylistById);
+app.get('/api/spotify/artist/:id', spotifyController.getArtistById);
+app.get('/api/spotify/artist/:id/top-tracks', spotifyController.getArtistTopTracks);
+app.get('/api/spotify/artist/:id/albums', spotifyController.getArtistAlbums);
+app.get('/api/spotify/artist/:id/singles', spotifyController.getArtistSingles);
+app.get('/api/spotify/artist/:id/compilations', spotifyController.getArtistCompilations);
+app.get('/api/spotify/artist/:id/appears-on', spotifyController.getArtistAppearsOn);
+app.get('/api/spotify/artist/:id/related-artists', spotifyController.getArtistRelatedArtists);
+app.get('/api/spotify/album/:id', spotifyController.getAlbumById);
+app.get('/api/spotify/search/:q', spotifyController.searchSpotify);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
